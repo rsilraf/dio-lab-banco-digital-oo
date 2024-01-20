1b5109059e6e6262288dee20f250785882255ce9
Rafael.Silva <rafael.silva@clearstrategy.ie>
2024-01-20 11:58:15 +0000

    Converte indentação para espaços

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,14 +1,14 @@
 
 public class Cliente {
 
-	private String nome;
+    private String nome;
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
 }
